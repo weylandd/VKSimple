8f94e5f6fcd16a46cef680c81bacdce86302beab
Alex Kopachev <jioruh2011@ya.ru>
2016-01-14 10:32:39 +0300

    create

---
+++ Example/Pods/Target Support Files/VKSimple/VKSimple-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_VKSimple : NSObject
+@end
+@implementation PodsDummy_VKSimple
+@end
---
+++ Example/Pods/Target Support Files/VKSimple/VKSimple-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double VKSimpleVersionNumber;
+FOUNDATION_EXPORT const unsigned char VKSimpleVersionString[];
+
