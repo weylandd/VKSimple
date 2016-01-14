464c2ff0f3a83d4d081f557e81a1fcc0e5fbbc01
Alex Kopachev <jioruh2011@ya.ru>
2016-01-14 10:41:18 +0300

    add first class

---
+++ Pod/Classes/VKSimpleSDK.h
@@ -0,0 +1,13 @@
+//
+//  VKSimpleSDK.h
+//  Pods
+//
+//  Created by Alex Kopachev on 14.01.16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VKSimpleSDK : NSObject
+
+@end
---
+++ Pod/Classes/VKSimpleSDK.m
@@ -0,0 +1,13 @@
+//
+//  VKSimpleSDK.m
+//  Pods
+//
+//  Created by Alex Kopachev on 14.01.16.
+//
+//
+
+#import "VKSimpleSDK.h"
+
+@implementation VKSimpleSDK
+
+@end
